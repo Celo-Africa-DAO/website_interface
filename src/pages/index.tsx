e92351662595a36e1a0eea672826c604d98d41b4
RonexLemon <ronexondimu@gmail.com>
2024-04-19 19:17:13 +0300

    fix:resolve curousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Together from "@/components/sections/Together";
 
 import Corousel from "@/components/ui/Cad/Corousel";
 
+
 export default function Home() {
   return (
     <div>
@@ -32,6 +33,7 @@ export default function Home() {
         <Together/>
         <Incubation/>
         <Explore/>
+       
         <UpcomingEvent/>
         <Incubation/>
       </>
