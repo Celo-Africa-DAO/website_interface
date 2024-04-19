275781303958ab2b266b21eb59c6291664cf3abf
Jordan <37406048+Jordan-type@users.noreply.github.com>
2024-04-19 23:38:15 +0300

    removed {/* <Together/> */}

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <>
         <Hero/>
         <Corousel/>
-        <Together/>
+        {/* <Together/> */}
         <Incubation/>
         <Team />
         <Explore/>
