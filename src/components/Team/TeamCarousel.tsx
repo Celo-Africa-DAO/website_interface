27e1e2ffd85a49286e58d2553f473cd669933d54
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-05-02 20:22:07 +0100

    Contributor update

--- src/components/Team/TeamCarousel.tsx
+++ src/components/Team/TeamCarousel.tsx
@@ -28,7 +28,7 @@ export function TeamCarousel({members, tab}: MembersProps) {
     <Carousel className="w-full ">
       <CarouselContent className="-ml-1">
         {members.map((member) => (
-          <CarouselItem key={member.id} className="pl-4 lg:pl-8 basis-[45%] md:basis-[30%] lg:basis-[25%]">
+          <CarouselItem key={member.id} className="pl-4 lg:pl-8 basis-[100%]  xsm:basis-[70%] sm:basis-[45%] md:basis-[30%] lg:basis-[25%]">
            <MemberCard tab={tab} member={member}/>
           </CarouselItem>
         ))}
