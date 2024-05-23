051fa6fa1bb837818ba54618dcfe3faea661e714
Muhindo-Galien <muhindogalien512@gmail.com>
2024-05-23 18:21:19 +0300

    incubator updated

--- src/components/sections/Incubation.tsx
+++ src/components/sections/Incubation.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Section from "../layouts/Section";
 import { Button } from "../ui/button";
+import { MdChevronRight } from "react-icons/md";
+
 
 const Incubation = () => {
   const handleLearnMore = () => {
@@ -17,45 +19,36 @@ const Incubation = () => {
   return (
     <div
       id="incubation"
-      className=" bg-cover bg-right md:bg-center bg-no-repeat px-3 md:px-7 xl:px-32 py-36 h-[260]"
+      className=" bg-cover  md:bg-center bg-no-repeat px-3 md:px-32 py-36 "
       style={{
-        backgroundImage: "url(img/incubation-bg.svg)",
+        backgroundImage: "url(./incubator-bg.png)",
       }}
     >
       <Section>
         <div className="flex flex-col gap-10">
-          <h2 className="text-4xl md:text-5xl text-[#FFFFFF] font-[100] font-gt-alpina-trial">
+          <h2 className="text-4xl md:text-5xl text-[#0C0C0C] font-[100] font-gt-alpina-trial">
             <span className="italic">Incubator</span> Program
           </h2>
-          <div className="flex flex-col md:flex-row justify-between  md:items-center gap-4 lg:gap-20">
-            <div className=" w-full  h-fit">
-              <img
-                src="roadmap-incubator-program-1.svg"
-                alt="incubator"
-                className="w-full h-fit"
-              />
-            </div>
-
-            <div className="w-[80%] sm:w-1/2">
-              <p className="mb-[20px] text-lg md:text-xl lg:text-2xl text-white font-light">
+          <div className="flex flex-col md:flex-row justify-between  md:items-center gap-4 sm:gap-20">
+            <div className="w-2/5 sm:w-2/4]">
+              <p className="mb-[20px] text-lg  text-[#4F4E4E] font-ligth">
                 Celo Africa DAO
                 <span className="font-semibold"> Incubator Program</span>, which
                 extends over a comprehensive{" "}
                 <span className="font-semibold">12-week duration, </span>
                 strategically crafted to empower and guide early-stage founders.
               </p>
-
-              <div className="flex flex-row gap- sm:gap-6">
-                <Button onClick={handleJoin} className=" rounded-[100px] bg-Celo-AD-yellow hover:bg-Celo-AD-yellow/75 text-[#090808] hover:bg-Celo-AD-yellow px-6 sm:px-10  w-fit mt-4 text-xs  sm:text-base">
+              <div className='flex flex-row sm:gap-4'>
+                <Button onClick={handleJoin}
+                  className=" rounded-[100px] bg-trasparent border border-[#090808] text-[#090808] hover:bg-Celo-AD-yellow px-6 sm:px-10  w-fit mt-4 text-xs  sm:text-base"
+                >
                   Join the cohort
                 </Button>
-                <Button onClick={handleLearnMore} className="bg-trasparent px-8 ms:px-10 w-fit mt-4 hover:bg-transparent text-xs  sm:text-base">
-                  Learn More{" "}
-                  <img
-                    src="./cheron-left-icon.png"
-                    className="ml-2"
-                    alt="left-icon"
-                  />
+                <Button onClick={handleLearnMore}
+                  className="bg-trasparent text-[#090808] border p-1  w-fit mt-4 hover:bg-transparent text-xs  sm:text-base"> <span className='px-6'>Learn More</span>
+                  <span className='bg-[#090808] p-1 rounded-full ml-2'>
+                    <MdChevronRight className='text-white' size={38} />
+                  </span>
                 </Button>
               </div>
             </div>
