905ee5566f8a22727fdb84b1ab952cd428c49ed1
Muhindo-Galien <muhindogalien512@gmail.com>
2024-05-27 12:22:07 +0300

    hero responsiveness adjusted

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,39 +7,46 @@ const Hero = () => {
 
 
     return (
-        <div id='home' className=" bg-Celo-AD-primary text-[#0C0C0C] pt-20">
+        <div id='home' className=" bg-Celo-AD-primary text-[#0C0C0C] pt-10 md:pt-20 ">
             <Section>
                 <div className='mx-6 xl:mx-0'>
-                    <div className='flex flex-col sm:flex-row justify-between  '>
-                        <div className='flex flex-col justify-center w-full md:w-3/5  text-left'>
-                            <h2 className='font-gt-alpina-trial  text-[1.9rem] sm:text-6xl text-[#0C0C0C]'>
-                                <span className='font-light'><i>Scaling</i><br /></span>Celo
-                                <span>Innovation<br /><i className='ml-14 xs:ml-16 ms:ml-20'>in Africa</i></span>
-                            </h2>
-                            <p className='text-[#565656] font-Inter py-8 w-full  sm:w-4/6 font-sans      text-base sm:text-lg'>A decentralized autonomous organization dedicated
+                    <div className='flex flex-col sm:flex-row justify-center  md:justify-between  '>
+                        <div className='flex flex-col justify-center  w-full md:w-3/5 text-center md:text-left'>
+                            <div className='flex flex-col justify-center font-gt-alpina-trial text-[2.8rem] sm:text-6xl text-[#0C0C0C]'>
+                                <p className='font-light md:text-left'><i>Scaling</i></p>
+                                <p className='-my-5 md:my-0'>Celo Innovation</p>
+                                <p>
+                                    <i className='ml-1 xs:ml-16 sm:ml-20  '>in Africa</i>
+                                </p>
+                            </div>
+
+                            <div className='md:hidden flex  my-12 sm:mb-12 md:mb-0 text-center justify-center '>
+                                <img src="./CAD-hero.png" alt="Celo_Africa_DAO" className='w-100 h-80' />
+                            </div>
+                            <p className='text-[#565656] font-Inter pb-8 md:py-8 w-full  sm:w-4/6 font-sans      text-base sm:text-lg'>A decentralized autonomous organization dedicated
                                 to fostering the growth and adoption of the CELO
                                 blockchain  across the Africa.
                             </p>
-                            <div className='flex flex-row sm:gap-4'>
+                            <div className='flex flex-row gap-4 justify-center md:justify-start'>
                                 <Button
-                                    className=" rounded-[100px] bg-Celo-AD-yellow hover:bg-Celo-AD-yellow/75 text-[#090808] hover:bg-Celo-AD-yellow px-6 sm:px-10  w-fit mt-4 text-xs  sm:text-base"
+                                    className=" rounded-[100px] bg-Celo-AD-yellow hover:bg-Celo-AD-yellow/75 text-[#090808] hover:bg-Celo-AD-yellow px-4 sm:px-10  w-fit mt-4 text-xs  sm:text-base"
                                 >
                                     Join our community
                                 </Button>
-                                <Button className="bg-trasparent text-[#090808] border p-1  w-fit mt-4 hover:bg-transparent text-xs  sm:text-base"> <span className='px-6'>Incubator program</span>
-                                    <span className='bg-[#090808] p-1 rounded-full ml-2'>
-                                        <MdChevronRight className='text-white' size={38}/>
+                                <Button className="bg-trasparent text-[#090808] border p-1  w-fit mt-4 hover:bg-transparent text-xs  sm:text-base"> <span className='px-4'>Incubator program</span>
+                                    <span className='bg-[#090808] p-1 rounded-full ml-1'>
+                                        <MdChevronRight className='text-white' size={28} />
                                     </span>
                                 </Button>
                             </div>
                         </div>
-                        <div className='w-2/5 order-first md:order-last mb-8 sm:mb-12 md:mb-0'>
+                        <div className='w-2/5 hidden md:block order-first md:order-last mb-8 sm:mb-12 md:mb-0'>
                             <img src="./CAD-hero.png" alt="Celo_Africa_DAO" className='cad' />
                         </div>
                     </div>
                 </div>
             </Section>
-            <StatsComponent/>
+            <StatsComponent />
         </div>
     )
 }
--- src/components/ui/Cad/StatsComponent.tsx
+++ src/components/ui/Cad/StatsComponent.tsx
@@ -2,39 +2,41 @@ import React from 'react';
 
 const StatsComponent = () => {
     return (
-        <div className="overflow-x-auto  max-w-7xl mx-auto">
-          <table className="min-w-full table-fixed">
-            <thead className=''>
-              <tr>
-                <th className="w-1/4"></th>
-                <th className="w-1/4"></th>
-                <th className="w-1/4"></th>
-                <th className="w-1/4"></th>
-              </tr>
-            </thead>
-            <tbody className=''>
-              <tr className="text-center ">
-                <td className="p-8 border-2 py-12 border-l-0 ">
-                  <p className="text-4xl font-semibold font-gt-alpina-trial">10+</p>
-                  <p className="text-gray-600 py-10">Sponsored events</p>
-                </td>
-                <td className="p-4 border-2">
-                  <p className="text-4xl font-semibold font-gt-alpina-trial">5k</p>
-                  <p className="text-gray-600 py-10">Community members</p>
-                </td>
-                <td className="p-4 border-2">
-                  <p className="text-4xl font-semibold font-gt-alpina-trial">10k +  </p>
-                  <p className="text-gray-600 py-10">Social meet ups</p>
-                </td>
-                <td className="p-4 border-2 border-r-0 ">
-                  <p className="text-4xl font-semibold font-gt-alpina-trial ">4</p>
-                  <p className="text-gray-600 py-10">Organised hackathons</p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="overflow-x-hiden max-w-7xl mx-auto p-4 -mt-10 md:mt-0">
+          <div className='-mx-10 md:mx-0 '>
+            <table className="min-w-full table-fixed transform-table">
+                <thead className='hidden md:table-header-group'>
+                    <tr>
+                        <th className="w-1/4"></th>
+                        <th className="w-1/4"></th>
+                        <th className="w-1/4"></th>
+                        <th className="w-1/4"></th>
+                    </tr>
+                </thead>
+                <tbody className='block md:table-row-group'>
+                    <tr className="block md:table-row text-center">
+                        <td className="p-8 border-2 py-12 border-l-0  border-r-0 border-b-0 md:border-b-2 md:w-1/4  block md:table-cell">
+                            <p className="text-4xl font-semibold font-gt-alpina-trial">10+</p>
+                            <p className="text-gray-600 py-10">Sponsored events</p>
+                        </td>
+                        <td className="p-4 border-2 md:border-r-2 border-l-0 md:border-l-2 border-r-0 border-b-0 md:border-b-2 md:w-1/4 block md:table-cell">
+                            <p className="text-4xl font-semibold font-gt-alpina-trial">5k</p>
+                            <p className="text-gray-600 py-10">Community members</p>
+                        </td>
+                        <td className="p-4 border-2 border-l-0 md:border-l-2 md:border-r-2 border-r-0 border-b-0 md:border-b-2 md:w-1/4 block md:table-cell">
+                            <p className="text-4xl font-semibold font-gt-alpina-trial">10k+</p>
+                            <p className="text-gray-600 py-10">Social meet ups</p>
+                        </td>
+                        <td className="p-4 border-2  border-l-0  border-r-0  md:border-b-2 md:w-1/4 block md:table-cell">
+                            <p className="text-4xl font-semibold font-gt-alpina-trial">4</p>
+                            <p className="text-gray-600 py-10">Organised hackathons</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+          </div>
         </div>
-      );
-    };
+    );
+};
 
 export default StatsComponent;
