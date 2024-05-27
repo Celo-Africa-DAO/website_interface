0a5a9d9c55c365f594c7db196c2e5112a658c914
khaddyBola <khadijahmusa232@gmail.com>
2024-05-27 11:34:35 +0100

    Merge branch 'develop' of https://github.com/Celo-Africa-DAO/website_interface into khaddybola/explore-section

--- src/components/ui/Card/PastEventCard.tsx
+++ src/components/ui/Card/PastEventCard.tsx
@@ -11,8 +11,8 @@ interface PastEventCardProps {
 }
 
 const PastEventCard: React.FC<PastEventCardProps> = ({ pastevent, onNext }) => (
-  <div className="px-32">
-    <h1 className="md:mb-[60px] mb-[45px] text-black font-extralight pt-16 font-gt-alpina italic text-[36px] md:text-[36px] leading-[45px] md:leading-[80px]">
+  <div className="px-10 md:px-6">
+    <h1 className="text-black font-extralight pt-16 font-gt-alpina italic text-[36px] md:text-[36px] leading-[45px] md:leading-[80px]">
       Past Event Video
     </h1>
     <div className="w-full h-1/4 flex md:flex-row flex-col justify-between items-center border-t-2 p-14 border-b-2 border-gray-500 mt-4">
@@ -27,9 +27,9 @@ const PastEventCard: React.FC<PastEventCardProps> = ({ pastevent, onNext }) => (
           {pastevent.date}
         </h3>
       </div>
-      <span className="flex md:justify-normal self-end md:self-auto h-[120px] w-[300px] bg-[#e9ecef]">
+      <span className="flex md:justify-normal self-end md:self-auto h-[120px] lg:w-[300px] w-[250px] bg-[#e9ecef]">
         <IoLogoYoutube
-          className="hidden md:block w-[50px] h-[50px] my-10 ml-32"
+          className="w-[50px] h-[50px] my-10 ml-32"
           color="red"
           onClick={onNext}
         />
@@ -37,7 +37,7 @@ const PastEventCard: React.FC<PastEventCardProps> = ({ pastevent, onNext }) => (
     </div>
     <div className="w-full h-1/4 flex md:flex-row flex-col justify-between md:p-[60px, 0px, 60px, 0px] items-center p-14 border-b-2 border-gray-500 mt-4">
       <div className="flex flex-col ">
-        <span className="text-2xl font-gt-alpina font-light text-black max-md:mt-7 mb-[16px] leading-[24px]">
+        <span className="text-2xl font-gt-alpina font-light text-black max-md:mt-7 mb-[20px] leading-[24px]">
           {pastevent.name}
         </span>
         <p className="font-bold self-start mb-[5px] hidden md:block text-[16px] text-black">
@@ -47,9 +47,9 @@ const PastEventCard: React.FC<PastEventCardProps> = ({ pastevent, onNext }) => (
           {pastevent.date}
         </h3>
       </div>
-      <span className="flex md:justify-normal self-end md:self-auto h-[120px] w-[300px] bg-[#e9ecef] ">
+      <span className="flex md:justify-normal self-end md:self-auto h-[120px] lg:w-[300px] w-[250px] bg-[#e9ecef]">
         <IoLogoYoutube
-          className="hidden md:block w-[50px] h-[50px] my-10 ml-32"
+          className="w-[50px] h-[50px] my-10 ml-32"
           color="red"
           onClick={onNext}
         />
