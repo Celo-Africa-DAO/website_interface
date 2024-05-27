3514b48f3a0fc91b3aa8c5001f0fb0e0fe886323
Muhindo-Galien <muhindogalien512@gmail.com>
2024-05-27 15:42:32 +0300

    fix: v2-teams init

--- src/components/Team/TeamCarousel.tsx
+++ src/components/Team/TeamCarousel.tsx
@@ -29,14 +29,14 @@ export function TeamCarousel({members, tab}: MembersProps) {
     <Carousel className="w-full ">
       <CarouselContent className="">
         {members.map((member) => (
-          <CarouselItem key={member.id} className="pl-4 lg:pl-8 basis-[100%] xsm:basis-[70%] sm:basis-[45%] md:basis-[30%] lg:basis-[25%]">
+          <CarouselItem key={member.id} className="basis-[100%] xsm:basis-[70%] sm:basis-[45%] md:basis-[30%] lg:basis-[25%]">
            <MemberCard tab={tab} member={member}/>
           </CarouselItem>
         ))}
       </CarouselContent>
       <div className="flex gap-2 mt-10 justify-end">
-        <CarouselPrevious className="static h-10 w-10 bg-transparent border-gray-300 hover:bg-Celo-AD-yellow" ><ChevronLeft className="h-4 w-4" /></CarouselPrevious>
-        <CarouselNext className="static h-10 w-10 bg-transparent border-gray-300 hover:bg-Celo-AD-yellow" ><ChevronRight className="h-4 w-4" /></CarouselNext>
+        <CarouselPrevious className="static h-10 w-10 bg-transparent border-gray-300 hover:bg-Celo-AD-yellow" ><ChevronLeft size={28} className="text-black" /></CarouselPrevious>
+        <CarouselNext className="static h-10 w-10 bg-transparent border-gray-300 hover:bg-Celo-AD-yellow" ><ChevronRight size={28} className="text-black" /></CarouselNext>
       </div>
     </Carousel>
   );
