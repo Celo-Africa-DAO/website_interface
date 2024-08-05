e0b96fe7ef00f9a7108e1d5fff9ff540c3a58cb3
Muhindo-Galien <muhindogalien512@gmail.com>
2024-08-05 11:11:59 -0700

    feat: init incubator page

---
+++ src/components/sections/IncubatorPath.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IncubatorPath = () => {
+  return (
+    <div>IncubatorPath</div>
+  )
+}
+
+export default IncubatorPath
---
+++ src/components/sections/IncubatorRoadMap.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IncubatorRoadMap = () => {
+  return (
+    <div>IncubatorRoadMap</div>
+  )
+}
+
+export default IncubatorRoadMap
