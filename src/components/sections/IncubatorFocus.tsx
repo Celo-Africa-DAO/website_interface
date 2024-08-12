e42d4a97e970477f059324cd761c7714ad11b776
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2024-08-12 12:57:33 +0100

    focusarea

--- src/components/sections/IncubatorFocus.tsx
+++ src/components/sections/IncubatorFocus.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import { HiMiniSquares2X2 } from "react-icons/hi2";
-import { LuSquareStack } from "react-icons/lu";
 
 const IncubatorFocus = () => {
   return (
@@ -23,8 +21,8 @@ const IncubatorFocus = () => {
           <div className="w-full md:w-[430px] p-[20px] md:p-[40px] flex flex-col gap-[32px] md:gap-[52px] border-[0.5px]">
           <Image src={'/support-for-early.svg'} width={46} height={46} alt="support for early stage icon" className="w-[36px] h-[36px] md:w-[46px] md:h-[46px]" />
           <div className="flex flex-col gap-[20px]">
-              <p className="font-GT-Alpina italic text-[24px] md:text-[32px]">
-                Support for Early-Stage Founders:
+              <p className="font-GT-Alpina italic text-[24px] md:text-[32px] leading-8">
+              Support for Early-Stage Founders:
               </p>
               <ul className="list-disc text-[16px] md:text-[20px] font-body font-normal text-[#655947] break-words">
                 <li>
@@ -42,7 +40,7 @@ const IncubatorFocus = () => {
           <div className="w-full md:w-[430px] p-[20px] md:p-[40px] flex flex-col gap-[32px] md:gap-[52px] border-[0.5px]">
             <Image src={'/investors-and-accelerator.svg'} width={46} height={46} alt="Investors and Accelerator Programs" className="w-[36px] h-[36px] md:w-[46px] md:h-[46px]" />
             <div className="flex flex-col gap-[20px]">
-              <p className="font-GT-Alpina italic text-[24px] md:text-[32px]">
+              <p className="font-GT-Alpina italic text-[24px] md:text-[32px] w-[359px] h-[64px] leading-8">
                 Investors and Accelerator Programs:
               </p>
               <ul className="list-disc text-[16px] md:text-[20px] font-body font-normal text-[#655947] break-words">
@@ -62,7 +60,7 @@ const IncubatorFocus = () => {
           <div className="w-full md:w-[430px] p-[20px] md:p-[40px] flex flex-col gap-[32px] md:gap-[52px] border-[0.5px]">
           <Image src={'/ecosystem-dev.svg'} width={46} height={46} alt="Ecosystem Development" className="w-[36px] h-[36px] md:w-[46px] md:h-[46px]" />
           <div className="flex flex-col gap-[20px]">
-              <p className="font-GT-Alpina italic text-[24px] md:text-[32px]">
+              <p className="font-GT-Alpina italic text-[24px] md:text-[32px] w-[359px] h-[64px]">
                 Ecosystem Development:
               </p>
               <ul className="list-disc text-[16px] md:text-[20px] font-body font-normal text-[#655947] break-words">
@@ -80,7 +78,7 @@ const IncubatorFocus = () => {
         </div>
       </div>
 
-      <div className="w-full flex items-center justify-center z-10 px-3 ">
+      <div className="w-full flex items-center justify-center z-10 px-3">
         <Image alt="" width={1000} height={1000} src="/incubatorSteps.png" />
       </div>
     </div>
