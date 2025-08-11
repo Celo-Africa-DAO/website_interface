9a9a2700e28341f24135f11e1e3b6a45949c1add
Muhindo-Galien <muhindogalien512@gmail.com>
2025-08-10 19:24:23 -0700

    feat: recreate startups API and utilities without sensitive data

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export interface Company {
+  id: string;
+  name: string;
+  location: string;
+  description: string;
+  batch: string;
+  tags: string[];
+  projectLink?: string;
+  founderContact?: string;
+}
+
+export interface SelectedFilters {
+  allCohorts: boolean;
+  selectedCohorts: string[];
+  allIndustries: boolean;
+  selectedIndustries: string[];
+  allCountries: boolean;
+  selectedCountries: string[];
+}
+
+export type SortOption =
+  | "Default"
+  | "Name"
+  | "Location"
+  | "cohort"
+  | "Vertical"
+  | "Country";
