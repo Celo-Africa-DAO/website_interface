6ba4ebfcb611533faae125853b577c78d8498172
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2025-10-01 12:24:00 +0100

    partner title added

--- src/components/incubator_v2/partner.tsx
+++ src/components/incubator_v2/partner.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export function Partner() {
     return (
+        <div className="flex flex-col items-center justify-center gap-8 ">
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-[50px] font-GT-Alpina font-thin text-center px-4">Incubator Partners</h2>
         <div className="flex flex-col md:flex-row justify-center  items-center gap-16 md:gap-8  md:w-3/4 text-black ">
+            
             <div onClick={() => window.open("https://verda.ventures/", "_blank")} className="cursor-pointer">
                 <Image
                     src="/verda3.svg"
@@ -30,6 +33,7 @@ export function Partner() {
                     height={40}
                 />
             </div>
+        </div>
         </div>
     );
 }
